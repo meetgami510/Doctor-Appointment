e0189af4db8401736771cf1ab086a347f1640377
Drumil_Akhenia <akheniad@gmail.com>
2023-04-17 17:48:04 +0530

    feat:login, redux & context in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useCookies } from 'react-cookie';
+import { useSelector } from "react-redux";
 import axios from 'axios';
+import CookiesProvider from "./context/CookiesProvider";
+import Spinner from "./components/Spinner";
+import Login from "./pages/Login";
+import { Provider } from 'react-redux';
+import store from "./redux/store"
 
 function App() {
+  const { loading } = useSelector(state => state.alerts);
+  const [cookies, setCookies, removeCookies] = useCookies(['token']);
+  const instance = axios.create({ baseURL: 'http://localhost:8080/api' });
   return (
-    <div>
-      hello
-    </div>
+    <CookiesProvider>
+      <BrowserRouter>
+        {loading && <Spinner />}
+        <Routes>
+          <Route path="/login" element={<Login instance={instance} />} />
+        </Routes>
+      </BrowserRouter>
+    </CookiesProvider>
   );
 }
 
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,49 @@
+import { useContext, useState } from "react"
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { hideLoading, showLoading } from "../redux/features/alertSlice";
+import axios from "axios";
+import { CookiesContext } from "../context/CookiesProvider";
+
+const Login = ({ instance }) => {
+    const { setCookies } = useContext(CookiesContext);
+    const [userData, setUserData] = useState({ email: '', password: '' });
+    const handleInputData = (event) => {
+        const { name, value } = event.target;
+        setUserData(prevState => ({ ...prevState, [name]: value }));
+    }
+    const { email, password } = userData;
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            dispatch(showLoading());
+            const res = await instance.post('/user/login', { email, password });
+            dispatch(hideLoading());
+            console.log(res.data);
+            if (res.data.success) {
+                setCookies('token', res.data.token);
+                alert('registerd successfully!');
+                navigate('/')
+            } else {
+                alert(res.data.message);
+            }
+        } catch (error) {
+            dispatch(hideLoading());
+            console.log(error);
+            alert('some thing went wrong');
+        }
+    }
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input type="text" value={email} name='email' onChange={handleInputData} />
+                <input type="password" name="password" value={password} onChange={handleInputData} />
+                <button type="submit">login</button>
+            </form>
+        </div>
+    )
+}
+
+export default Login;
